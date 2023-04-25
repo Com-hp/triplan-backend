5c6dffd08c963bc234afc95bb4c121ae27d52279
안수민 <20201052@vision.hoseo.edu>
2023-04-25 20:01:06 +0900

    git commit

---
+++ triplan/src/test/java/irlab/triplan/TriplanApplicationTests.java
@@ -0,0 +1,13 @@
+package irlab.triplan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TriplanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

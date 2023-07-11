2db27e5ccb0d6cd345a3f8a40e90764033b7778f
안수민 <20201052@vision.hoseo.edu>
2023-07-11 15:38:27 +0900

    Revert "trip API - 여행 목록(그룹탭), 여행 목록(홈) 수정 / 여행 생성 추가"
    This reverts commit 5bf91fb375673ad3e5c78d4434a245bf8f4ecc09.

--- triplan/src/main/java/irlab/triplan/config/Configurer.java
+++ triplan/src/main/java/irlab/triplan/config/Configurer.java
@@ -1,17 +1,13 @@
-//package irlab.triplan.config;
-//
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-//import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//
-//@Configuration
-//@EnableWebMvc
-//public class Configurer implements WebMvcConfigurer {
-//    @Override
-//    public void addResourceHandlers(ResourceHandlerRegistry registry){
-//        registry
-//                .addResourceHandler("/upload/**") //외부 경로 매핑
-//                .addResourceLocations("classpath:/memo/"); //외부 경로
-//    }
-//}
+package irlab.triplan.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class Configurer implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry){
+        registry.addResourceHandler("/images/**").addResourceLocations("file:///home/hoseolab420/triplan/resources");
+    }
+}
